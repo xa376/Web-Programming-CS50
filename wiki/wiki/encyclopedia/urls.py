e84e9dfd0ea37a96d2d9dee1d4883587b94eef70
unknown <projectfigirl@gmail.com>
2022-09-04 19:48:58 -0400

    Markdown Issue Break

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
+from . import util
+
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("entry", views.entry, name="entry")
 ]
+for entry in util.list_entries():
+    urlpatterns.append(path(entry, views.entry, name=entry))
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
+from django import forms
+import markdown2
 
 from . import util
 
+class NewTaskForm(forms.Form):
+    formEntry = forms.CharField(label="")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewTaskForm()
     })
 
+def entry(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            formEntry = form.cleaned_data["formEntry"]
+            if util.get_entry(formEntry):
+                return HttpResponseRedirect(formEntry)
+            searchMatches = []
+            for listEntry in util.list_entries():
+                if formEntry.lower() in listEntry.lower():
+                    searchMatches.append(listEntry)
+            return render(request, "encyclopedia/results.html", {
+                "matches": searchMatches
+            })
+
+    for listEntry in util.list_entries():
+        if listEntry in request.resolver_match:
+            return render(request, "encyclopedia/error.html")
+            return render(request, "encyclopedia/entry.html", {
+                "markdownText": markdown2.markdown(util.get_entry(listEntry))
+            })
+    
+    return render(request, "encyclopedia/entry.html", {
+        "testing": request
+    })
